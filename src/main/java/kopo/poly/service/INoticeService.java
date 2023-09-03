cac4f9a8466bc3beddf6f39320199999c06c653e
김지태 <2220110012@gspace.kopo.ac.kr>
2023-09-03 16:06:59 +0900

    2023.09.03

--- src/main/java/kopo/poly/service/INoticeService.java
+++ src/main/java/kopo/poly/service/INoticeService.java
@@ -14,6 +14,30 @@ public interface INoticeService {
     /**
      * 공지사항 상세 정보가져오기
      *
-     * @Param pDTO 공지
+     * @param pDTO 공지사항 상세 가져오기 위한 정보
+     * @param type 조회수 증가여부(true : 증가, false : 증가안함
      */
+    NoticeDTO getNoticeInfo(NoticeDTO pDTO, boolean type) throws Exception;
+
+    /**
+     * 해당 공지사항 수정하기
+     *
+     * @param pDTO 공지사항 수정하기 위한 정보
+     */
+    void updateNoticeInfo(NoticeDTO pDTO) throws Exception;
+
+    /**
+     * 해당 공지사항 삭제하기
+     *
+     * @param pDTO 공지사항 삭제하기 위한 정보
+     */
+    void deleteNoticeInfo(NoticeDTO pDTO) throws Exception;
+
+    /**
+     * 해당 공지사항 저장하기
+     *
+     * @param pDTO 공지사항 저장하기 위한 정보
+     */
+    void insertNoticeInfo(NoticeDTO pDTO) throws Exception;
+
 }
