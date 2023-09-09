365ce6eec6d111e5fcced75f5b01f1ab647d53ff
김지태 <2220110012@gspace.kopo.ac.kr>
2023-09-09 12:29:18 +0900

    2023.09.09

--- src/main/java/kopo/poly/dto/NoticeDTO.java
+++ src/main/java/kopo/poly/dto/NoticeDTO.java
@@ -1,6 +1,5 @@
 package kopo.poly.dto;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,25 +7,17 @@ import lombok.Setter;
 @Setter
 public class NoticeDTO {
 
-    private long noticeSeq; // 기본키, 순번
-
+    private Long noticeSeq; // 기본키, 순번
     private String title; // 제목
-
     private String noticeYn; // 공지글 여부
-
     private String contents; // 글 내용
-
     private String userId; // 작성자
-
     private String readCnt; // 조회수
-
     private String regId; // 등록자 아이디
-
     private String regDt; // 등록일
-
     private String chgId; // 수정자 아이디
-
-    private String chgDT; // 수정일
+    private String chgDt; // 수정일
 
     private String userName; // 등록자명
+
 }
