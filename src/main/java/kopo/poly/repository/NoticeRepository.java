daa6d5c859ee10f79810e5faeaf2b08d852c012c
김지태 <2220110012@gspace.kopo.ac.kr>
2023-09-19 11:25:12 +0900

    2023.09.19

--- src/main/java/kopo/poly/repository/NoticeRepository.java
+++ src/main/java/kopo/poly/repository/NoticeRepository.java
@@ -1,6 +1,5 @@
 package kopo.poly.repository;
 
-
 import kopo.poly.repository.entity.NoticeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -21,18 +20,18 @@ public interface NoticeRepository extends JpaRepository<NoticeEntity, Long> {
     /**
      * 공지사항 리스트
      *
-     * @Param noticeSeq 공지사항 PK
+     * @param noticeSeq 공지사항 PK
      */
     NoticeEntity findByNoticeSeq(Long noticeSeq);
 
     /**
      * 공지사항 상세 보기할 때, 조회수 증가하기
      *
-     * @Param noticeSeq 공지사항 PK
+     * @param noticeSeq 공지사항 PK
      */
-
     @Modifying(clearAutomatically = true)
     @Query(value = "UPDATE NOTICE A SET A.READ_CNT = IFNULL(A.READ_CNT, 0) + 1 WHERE A.NOTICE_SEQ = ?noticeSeq",
-    nativeQuery = true)
-    int updateReadCnt(@Param(value= "noticeSeq") Long noticeSeq);
+            nativeQuery = true)
+    int updateReadCnt(@Param(value = "noticeSeq") Long noticeSeq);
+
 }
